7f903fd6db32ce967ec98369f0978f0a7cb24fd6
Preston Engstrom <p.engstrom94@gmail.com>
2016-03-14 16:40:04 -0400

    plotting works, fixed the stupid class count issue

--- Source/k_means.py
+++ Source/k_means.py
@@ -2,30 +2,20 @@ import numpy as np
 import pandas as pd
 import math
 import pprint
-
-# select data file (s1, s2, s3, s4)
-# select k: number of means
-# select initialization
-
-# init starting centroids
-# LABEL A
-# for each point, calculate nearest centroid
-# calculate average of points in each cluster, make that the new centroid
-# GOTO A until no change
-
+from  matplotlib import pyplot
+import seaborn
 
 data_path = "../data/"
 
-
-data_points = []
-means = []
-centroid_sums = []
 
 
 def euclid_dist(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1] - p2[1])**2)
 
 def k_means(k, data_file, dist_function):
+    data_points = []
+    means = []
+    centroid_sums = []
 
     with open(data_path+data_file) as f:
         f.readline()
@@ -35,59 +25,64 @@ def k_means(k, data_file, dist_function):
 
         # select random data points as mean
         clss = 0
+        num_in_class = []
         for mean in xrange(k):
             ex = np.random.random_integers(0, len(data_points))
             p = data_points[ex][0]
             means.append([p[0], p[1], clss])
             centroid_sums.append([0, 0, clss])
+            num_in_class.append(0)
             clss += 1
-
-        print means
 
         epocs = 0
         no_change = True
+
         while no_change:
             epocs += 1
-            change_made = False
 
             # reset collector for centroid sums
             for centro in centroid_sums:
                 centro[0] = 0.0
                 centro[1] = 0.0
 
+            for i in xrange(len(num_in_class)):
+                num_in_class[i] = 0
+
+            # set flag to test for convergence
+            no_change = False
+
             for rec in data_points:
 
                 for index in xrange(len(means)):
                     dist = dist_function(rec[0], means[index])
-                    print index, dist
                     if dist < rec[2]:
-                        change_made = True
+                        no_change = True
                         rec[1] = index
                         rec[2] = dist
-                    centroid_sums[rec[1]][0] += rec[0][0]
-                    centroid_sums[rec[1]][1] += rec[0][1]
 
-                if not change_made:
-                    no_change = False
-                    break
+                num_in_class[rec[1]] += 1
+                centroid_sums[rec[1]][0] += rec[0][0]
+                centroid_sums[rec[1]][1] += rec[0][1]
 
-            if not change_made:
-                break
+
 
             index = 0
             for centro in centroid_sums:
-                centro[0] /= len(data_points)
-                centro[1] /= len(data_points)
-                print centro
+                centro[0] /= num_in_class[index]
+                centro[1] /= num_in_class[index]
                 means[index][0] = centro[0]
                 means[index][1] = centro[1]
                 index += 1
 
+
+
     out_data = [[], means]
+
     for rec in data_points:
         del rec[2]
         out_data[0].append([rec[0][0], rec[0][1], rec[1]])
-
+    print len(data_points)
+    print num_in_class
     return out_data
 
 
@@ -96,7 +91,14 @@ def k_means(k, data_file, dist_function):
 
 
 
-dat = k_means(6, "s1.txt", euclid_dist)
-pprint.pprint(dat)
+dat = k_means(5, "s3.txt", euclid_dist)
 
+pprint.pprint(dat[0])
+data_points = pd.DataFrame.from_records(dat[0],columns=['x', 'y', 'class'])
+fg = seaborn.FacetGrid(data=data_points, hue='class', size=6, aspect=1, )
+fg.set(yticklabels=[])
+fg.set(xticklabels=[])
+fg.despine()
+fg.map(pyplot.scatter, 'x', 'y', s=5)
+fg.savefig("s3_1.png",  bbox_inches='tight')
 
